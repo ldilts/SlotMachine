e68f6a10f35c0ae7fdd7f893c3506518c1c3796f
lditls <Lucas@Lucass-Notebook.local>
2014-10-15 11:35:35 -0300

    Additional UI
    Added card views;
    Added score views;
    Additional UI tweaks.

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -16,6 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        var navigationBarAppearace = UINavigationBar.appearance()
+        
+        UIApplication.sharedApplication().statusBarStyle = UIStatusBarStyle.LightContent
+        
         return true
     }
 
--- ViewController.swift
+++ ViewController.swift
@@ -7,20 +7,53 @@
 //
 
 import UIKit
+import QuartzCore
 
 class ViewController: UIViewController {
     
-    var firstContainer: UIView!
-    var secondContainer: UIView!
-    var thirdContainer: UIView!
-    var fourthContainer: UIView!
+    var bottomButtonContainer: UIView!
+    var cardContainer: UIView!
+    var gameResultsContainer: UIView!
+    var divideContainer: UIView!
+    
+    var creditsLabel:UITextView!
+    var betLabel:UITextView!
+    var paidLabel:UITextView!
+    var creditsTitleLabel:UILabel!
+    var betTitleLabel:UILabel!
+    var paidTitleLabel:UILabel!
+    
     
     // Making CGFloat constant 
     // k stands for constant
-    let kMarginForView:CGFloat = 10.0
+//    let kMarginForView:CGFloat = 10.0
     let kSixth:CGFloat = 1.0/6.0
-    let kBottom:CGFloat = 966/1136
+//    let kBottom:CGFloat = 966/1136
     let kBottomHeight:CGFloat = 170/1136
+    let kCardContainerHeight:CGFloat = 720/1136
+    let kCardRows:CGFloat = 3
+    let kCardColumns:CGFloat = 3
+    
+    
+    let kGameResultsOffsetFromEdge:CGFloat = 40/640
+    let kGameResultsOffsetFromEdge2:CGFloat = 260/640
+    let kGameResultsOffsetFromEdge3:CGFloat = 480/640
+    let kGameResultsOffsetFromEdgeTop:CGFloat = 16/1136
+    let kGameResultsOffsetFromOtherView:CGFloat = 100/640
+    
+    
+    let kCardOffsetFromEdge:CGFloat = 40/640
+    let kCardOffsetFromOtherCard:CGFloat = 55/640
+    let kCardOffsetFromOtherCardTop:CGFloat = 10/1136
+    
+    let kCardWidth:CGFloat = 150/640
+    let kCardHeight:CGFloat = 210/1136
+    
+    
+    let kNumberOfContainers = 3
+    let kNumberOfSlots = 3
+    let kThird:CGFloat = 1.0/3.0
+    let kMarginForSlot:CGFloat = 2.0
     
     let backgroundColor = UIColor(hex: 0xFCF2D9)
     let titleColor = UIColor(hex: 0xFAD961)
@@ -35,13 +68,12 @@ class ViewController: UIViewController {
         let titleDict: NSDictionary = [NSForegroundColorAttributeName: titleColor, NSFontAttributeName: UIFont(name: "Streetwear", size: 24)]
         self.navigationController?.navigationBar.titleTextAttributes = titleDict
         
-//        println("\(self.navigationController?.navigationBar.frame.height)")
-//    
-        var barHeight = self.navigationController?.topLayoutGuide.length
-        
-        var aux:CGFloat = barHeight!
+        self.view.backgroundColor = backgroundColor
+        self
         
         setupContainerViews()
+        self.setupCardContainer(cardContainer)
+        self.setupGameResultsContainer()
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -57,17 +89,112 @@ class ViewController: UIViewController {
         
         let navigationBarHeight: CGFloat = (self.navigationController?.navigationBar.bounds.height)!
         let statusBarHeight: CGFloat = (self.navigationController?.topLayoutGuide.length)!
+        
+        self.cardContainer = UIView(frame: CGRect(x: self.view.frame.origin.x, y: self.view.frame.origin.y, width: self.view.frame.width, height: self.view.frame.height * kCardContainerHeight))
+//        self.cardContainer.backgroundColor = UIColor.blueColor()
+        self.view.addSubview(self.cardContainer)
+        
+        self.bottomButtonContainer = UIView(frame: CGRect(x: 0, y: self.view.frame.height - navigationBarHeight - statusBarHeight - (self.view.frame.height * kBottomHeight), width: self.view.frame.width, height: self.view.frame.height * kBottomHeight))
+        self.bottomButtonContainer.backgroundColor = navigationBarColor
+        self.view.addSubview(self.bottomButtonContainer)
+        
+        self.gameResultsContainer = UIView(frame: CGRect(x: self.view.frame.origin.x, y: self.cardContainer.frame.height, width: self.view.frame.width, height: self.view.frame.height - self.cardContainer.frame.height - self.bottomButtonContainer.frame.height - navigationBarHeight - statusBarHeight))
+//        self.gameResultsContainer.backgroundColor = UIColor.blackColor()
+        self.view.addSubview(self.gameResultsContainer)
+        
+        self.divideContainer = UIView(frame: CGRect(x: self.view.frame.origin.x, y: self.cardContainer.frame.height, width: self.view.frame.width, height: 0.5))
+        self.divideContainer.backgroundColor = navigationBarColor
+        self.view.addSubview(self.divideContainer)
+        
+    }
     
-        self.view.backgroundColor = backgroundColor
+    func setupCardContainer(containerView: UIView) {
         
-//        self.firstContainer = UIView(frame: CGRect(x: 0, y: self.view.frame.height - navigationBarHeight - statusBarHeight - (self.view.frame.height * kBottomHeight), width: self.view.frame.width, height: self.view.frame.height * kBottomHeight))
-//        self.firstContainer.backgroundColor = navigationBarColor
-//        self.view.addSubview(self.firstContainer)
+        let cardWidth:CGFloat = self.view.frame.width * kCardWidth
+        let cardHeight:CGFloat = self.view.frame.height * kCardHeight
+        let edgeOffset:CGFloat = self.view.frame.width * kCardOffsetFromEdge
+        let offsetFromOtherCard:CGFloat = self.view.frame.width * kCardOffsetFromOtherCard
+        let offsetFromOtherCardTop:CGFloat = self.view.frame.height * kCardOffsetFromOtherCardTop
         
-        self.firstContainer = UIView(frame: CGRect(x: 0, y: self.view.frame.height - navigationBarHeight - statusBarHeight - (self.view.frame.height * kBottomHeight), width: self.view.frame.width, height: self.view.frame.height * kBottomHeight))
-        self.firstContainer.backgroundColor = navigationBarColor
-        self.view.addSubview(self.firstContainer)
+        for var i:CGFloat = 0; i < kCardRows; i++ {
+            for var j:CGFloat = 0; j < kCardColumns; j++ {
+                var cardImageView = UIImageView()
+                cardImageView.layer.cornerRadius = 5.0
+                cardImageView.layer.masksToBounds = true
+                
+                cardImageView.layer.borderWidth = 0.5
+                cardImageView.layer.borderColor = (navigationBarColor).CGColor
+//                cardImageView.layer.shadowOffset = CGSizeMake(0, 0);
+//                cardImageView.layer.shadowOpacity = 0.8;
+//                cardImageView.layer.shadowRadius = 5.0;
+//                cardImageView.backgroundColor = UIColor.blueColor()
+
+                switch i {
+                    case 0:
+                        cardImageView.frame = CGRect(x: edgeOffset, y: edgeOffset + (j * cardHeight) + (j * offsetFromOtherCardTop), width: cardWidth, height: cardHeight)
+                    case 1:
+                        cardImageView.frame = CGRect(x: edgeOffset + cardWidth + offsetFromOtherCard, y: edgeOffset + (j * cardHeight) + (j * offsetFromOtherCardTop), width: cardWidth, height: cardHeight)
+                    case 2:
+                        cardImageView.frame = CGRect(x: edgeOffset + cardWidth + offsetFromOtherCard + cardWidth + offsetFromOtherCard, y: edgeOffset + (j * cardHeight) + (j * offsetFromOtherCardTop), width: cardWidth, height: cardHeight)
+                    default:
+                        cardImageView.frame = CGRect(x: 0, y: 0, width: cardWidth, height: cardHeight)
+                }
+                self.cardContainer.addSubview(cardImageView)
+            }
+        }
+    }
+    
+    func setupGameResultsContainer () {
+        self.creditsLabel = UITextView()
+        self.creditsLabel.layer.cornerRadius = 5.0
+        self.creditsLabel.backgroundColor = UIColor.whiteColor()
+        self.creditsLabel.layer.borderWidth = 0.5
+        self.creditsLabel.layer.borderColor = (navigationBarColor).CGColor
         
+        self.creditsLabel.text = "000000"
+        self.creditsLabel.textColor = navigationBarColor
+        self.creditsLabel.font = UIFont(name: "Menlo-Bold", size: 14)
+        self.creditsLabel.textAlignment = NSTextAlignment.Center
+        
+        self.creditsLabel.frame = CGRect(x: (self.view.frame.width * kGameResultsOffsetFromEdge), y: (self.view.frame.width * kGameResultsOffsetFromEdgeTop), width: 60, height: 25)
+//        self.creditsLabel.sizeToFit()
+        
+//        self.creditsLabel.center = CGPoint(x: (self.view.frame.width * kGameResultsCenterOffsetFromEdge), y: (self.view.frame.height * kGameResultsCenterOffsetFromEdgeTop))
+        self.gameResultsContainer.addSubview(self.creditsLabel)
+        
+
+        self.betLabel = UITextView()
+        self.betLabel.layer.cornerRadius = 5.0
+        self.betLabel.backgroundColor = UIColor.whiteColor()
+        self.betLabel.layer.borderWidth = 0.5
+        self.betLabel.layer.borderColor = (navigationBarColor).CGColor
+        
+        self.betLabel.text = "000000"
+        self.betLabel.textColor = navigationBarColor
+        self.betLabel.font = UIFont(name: "Menlo-Bold", size: 14)
+        self.betLabel.textAlignment = NSTextAlignment.Center
+//        self.betLabel.sizeToFit()
+        
+//        self.betLabel.center = CGPoint(x: (self.view.frame.width * kGameResultsOffsetFromEdge2), y: (self.view.frame.width * kGameResultsOffsetFromEdgeTop))
+        self.betLabel.frame = CGRect(x: (self.view.frame.width * kGameResultsOffsetFromEdge2), y: (self.view.frame.width * kGameResultsOffsetFromEdgeTop), width: 60, height: 25)
+        self.gameResultsContainer.addSubview(self.betLabel)
+        
+        
+        self.paidLabel = UITextView()
+        self.paidLabel.layer.cornerRadius = 5.0
+        self.paidLabel.backgroundColor = UIColor.whiteColor()
+        self.paidLabel.layer.borderWidth = 0.5
+        self.paidLabel.layer.borderColor = (navigationBarColor).CGColor
+        
+        self.paidLabel.text = "000000"
+        self.paidLabel.textColor = navigationBarColor
+        self.paidLabel.font = UIFont(name: "Menlo-Bold", size: 14)
+        self.paidLabel.textAlignment = NSTextAlignment.Center
+//        self.paidLabel.sizeToFit()
+        
+//        self.paidLabel.center = CGPoint(x: (self.view.frame.width * kGameResultsOffsetFromEdge3), y: (self.view.frame.width * kGameResultsOffsetFromEdgeTop))
+        self.paidLabel.frame = CGRect(x: (self.view.frame.width * kGameResultsOffsetFromEdge3), y: (self.view.frame.width * kGameResultsOffsetFromEdgeTop), width: 60, height: 25)
+        self.gameResultsContainer.addSubview(self.paidLabel)
     }
 
 }
